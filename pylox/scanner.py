650c227fe5466bbc3f4d7a7836144feaf0d99c59
Rohan Almeida <rohan.almeida@gmail.com>
2025-12-28 21:11:35 +0530

    one or two character tokens

--- pylox/scanner.py
+++ pylox/scanner.py
@@ -40,6 +40,29 @@ class Scanner():
             case '+': self.add_token(TokenType.PLUS)
             case ';': self.add_token(TokenType.SEMICOLON)
             case '*': self.add_token(TokenType.STAR)
+
+            # single or double character tokens
+            case '!':
+                if self.match('='):
+                    self.add_token(TokenType.BANG_EQUAL)
+                else:
+                    self.add_token(TokenType.BANG)
+            case '=':
+                if self.match('='):
+                    self.add_token(TokenType.EQUAL_EQUAL)
+                else:
+                    self.add_token(TokenType.EQUAL)
+            case '>':
+                if self.match('='):
+                    self.add_token(TokenType.GREATER_EQUAL)
+                else:
+                    self.add_token(TokenType.GREATER)
+            case '<':
+                if self.match('='):
+                    self.add_token(TokenType.LESS_EQUAL)
+                else:
+                    self.add_token(TokenType.LESS)
+                    
             case _:
                 self.report_error(self.line, "", f"Unexpected character: {c}")
             
@@ -51,3 +74,12 @@ class Scanner():
     def add_token(self, token_type: TokenType, literal: object = None):
         text = self.source[self.start:self.current]
         self.tokens.append(Token(token_type, text, literal, self.line))
+
+    def match(self, expected: str) -> bool:
+        if self.is_at_end():
+            return False
+        if self.source[self.current] != expected:
+            return False
+
+        self.current += 1
+        return True
